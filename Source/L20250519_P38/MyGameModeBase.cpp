224cdc27cc118c154923ad34fbd3d5188c437b4a
junios-study <31557850+junios-study@users.noreply.github.com>
2025-05-21 13:53:37 +0900

    C++로 프로젝트 변경

---
+++ Source/L20250519_P38/MyGameModeBase.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+#include "MyPlayerController.h"
+#include "MyPawn.h"
+
+//이건 모하는거야? CDO 기본값
+AMyGameModeBase::AMyGameModeBase()
+{
+	UE_LOG(LogTemp, Warning, TEXT("안녕하세요."));
+
+	if (GEngine)
+	{ 
+		GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red, TEXT("Hello World")); 
+	}
+
+	PlayerControllerClass = AMyPlayerController::StaticClass();
+	DefaultPawnClass = AMyPawn::StaticClass();
+}
