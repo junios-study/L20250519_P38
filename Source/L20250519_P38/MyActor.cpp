f5cdd00f9a74348022192a3669c074b2300a4ffe
junios-study <31557850+junios-study@users.noreply.github.com>
2025-05-21 16:39:38 +0900

    Rocket CPP로 만들기

---
+++ Source/L20250519_P38/MyActor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = Box;
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	Body->SetupAttachment(Box);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Body(TEXT("/Script/Engine.StaticMesh'/Game/P38/Meshes/SM_Rocket.SM_Rocket'"));
+	if (SM_Body.Succeeded())
+	{
+		Body->SetStaticMesh(SM_Body.Object);
+	}
+	Body->SetRelativeRotation(FRotator(-90.0f, 0, 0));
+
+	Movement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Movement"));
+
+	Movement->InitialSpeed = 2000.0f;
+	Movement->MaxSpeed = 2000.0f;
+	Movement->ProjectileGravityScale = 0.0f;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/L20250519_P38/MyActor.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+class UProjectileMovementComponent;
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class L20250519_P38_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Compoennts")
+	TObjectPtr<UBoxComponent> Box;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Compoennts")
+	TObjectPtr<UStaticMeshComponent> Body;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Compoennts")
+	TObjectPtr<UProjectileMovementComponent> Movement;
+};
